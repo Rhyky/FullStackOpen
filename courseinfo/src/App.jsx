60b43985c345efd67833d0f8d9c5a342dd4707bf
Tiago <tiagomanuel7@msn.com>
2026-03-01 10:03:46 +0000

    finishedExercices

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = (t) =>{
+  return(
+  <h1>{t.course}</h1>
+  )
+}
+
+const Content = (c) =>{
+  return(
+    <>
+    <Exer part={c.part1} exer={c.exer1}/>
+    <Exer part={c.part2} exer={c.exer2}/>
+    <Exer part={c.part3} exer={c.exer3}/>
+    </>
+  )
+}
+
+const Exer = (e) =>{
+  return(
+    <p>
+      {e.part}{e.exer}
+    </p>    
+  )
+}
+
+const Total = (t) => {
+  return(
+    <p>Number of exercises {t.exer1+t.exer2+t.exer3}</p>
+  )
+}
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using pops to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+   <div>
+    <Header course={course}/>
+    <Content part1={part1} part2={part2} part3={part3}
+      exer1={exercises1} exer2={exercises2} exer3={exercises3}/>
+    <Total exer1={exercises1} exer2={exercises2} exer3={exercises3}/>
+   </div>
+  )
+}
+
+export default App
