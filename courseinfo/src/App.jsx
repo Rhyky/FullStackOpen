de263739d532b658a12439ee8dca9895f25c1cce
Tiago <tiagomanuel7@msn.com>
2026-03-02 09:42:54 +0000

    change const parts to array

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -24,31 +24,32 @@ const Part = (e) =>{
 
 const Total = (t) => {
   return(
-    <p>Number of exercises {t.exer1+t.exer2+t.exer3}</p>
+    <p>Number of exercises {t.part[0].exercises+t.part[1].exercises+t.part[2].exercises}</p>
   )
 }
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }  
-  const part2 = {
-    name: 'Using pops to pass data',
-    exercises: 7
-  }  
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-}
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
    <div>
     <Header course={course}/>
-    <Content part1={part1.name} part2={part2.name} part3={part3.name}
-      exer1={part1.exercises} exer2={part2.exercises} exer3={part3.exercises}/>
-    <Total exer1={part1.exercises} exer2={part2.exercises} exer3={part3.exercises}/>
+    <Content part={parts}/>
+    <Total part={parts}/>
    </div>
   )
 }
